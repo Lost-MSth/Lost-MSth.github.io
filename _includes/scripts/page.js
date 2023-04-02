024fb72242785b6bad4f12cbb674b0d0d9ced0c3
Lost-MSth <th84292@foxmail.com>
2023-04-03 00:00:57 +0800

    Add scroll button

--- _includes/scripts/page.js
+++ _includes/scripts/page.js
@@ -47,3 +47,33 @@ function toggleTheme() {
         document.getElementById("themeSwitch2").className = "fas fa-sun";
     }
 }
+
+
+
+var SOURCES = window.TEXT_VARIABLES.sources;
+window.Lazyload.js(SOURCES.jquery, function () {
+    const check_if_in_view = function () {
+        var scrollTop = $(this).scrollTop();
+        var scrollHeight = $(document).height();
+        var windowHeight = $(this).height();
+        scrollTop < 100
+            ? $('#scroll_up_button').hide('fast')
+            : $('#scroll_up_button').show('fast')
+        if (Math.abs(Math.round(scrollTop + windowHeight) - scrollHeight) < 100) {// 滚动到底部
+            $('#scroll_down_button').hide('fast')
+        } else {
+            $('#scroll_down_button').show('fast')
+        }
+    }
+    check_if_in_view();
+    $('#scroll_up_button').click(function () {
+        $('html, body').animate({ scrollTop: '0px' }, 600)
+    })
+
+    $('#scroll_down_button').click(function () {
+        $('html, body').animate({ scrollTop: $(document).height() }, 600)
+    })
+
+    $(window).bind('scroll', check_if_in_view)
+});
+
